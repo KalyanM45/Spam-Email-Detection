bae290f9820d65f92774c6a9e418955dad365f3e
KalyanM45 <mhemakalyan@gmail.com>
2025-12-25 22:28:49 +0530

    Dependecies Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './',
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
   server: {
     port: 3000,
     proxy: {
